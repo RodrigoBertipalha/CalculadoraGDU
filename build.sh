7691a88a16fb3ec3708d7bcd13af18ffc9731a90
Rodrigo Bertipalha <seu-email@exemplo.com>
2025-08-20 00:50:47 -0300

    Corrige dependências no requirements.txt e build.sh para deploy no Render

--- build.sh
+++ build.sh
@@ -3,5 +3,14 @@
 # Exit on error
 set -o errexit
 
-# Install dependencies
+# Upgrade pip
+pip install --upgrade pip
+
+# Install dependencies from requirements.txt
 pip install -r requirements.txt
+
+# Garantir que xlsxwriter seja instalado (como precaução extra)
+pip install xlsxwriter
+
+# Imprimir as dependências instaladas para verificação
+pip list
